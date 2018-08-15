d98d936973360e6eea8fd6e4cb121fd43d116ac9
OriginCode <origincoder@yahoo.com>
2018-08-15 17:51:54 +0800

    creat variable PORT

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from wsgiref.simple_server import make_server
 
+PORT = 8000 # Default Port
+
 def captive_portal(environ, responce):
     responce('204 No Content', [('Content-Type', 'text/html')])
     return [b'<h1>OriginCode\'s Google Capitive Portal (204).']
 
-httpd = make_server('', 8000, captive_portal)
-print('Started serving on port 8000...')
+httpd = make_server('', PORT, captive_portal)
+print('Started serving on port %d...' % PORT)
 httpd.serve_forever()
